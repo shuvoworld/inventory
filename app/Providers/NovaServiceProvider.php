7553d6e16798e42dc6c97ff3a1d8c709acbccde9
shuvoworld <shuvoworld@gmail.com>
2024-06-20 14:08:17 +0600

    update resource and add permission

---
+++ app/Providers/NovaServiceProvider.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Laravel\Nova\Nova;
+use Laravel\Nova\NovaApplicationServiceProvider;
+use Sereny\NovaPermissions\NovaPermissions;
+
+class NovaServiceProvider extends NovaApplicationServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+    }
+
+    /**
+     * Register the Nova routes.
+     *
+     * @return void
+     */
+    protected function routes()
+    {
+        Nova::routes()
+                ->withAuthenticationRoutes()
+                ->withPasswordResetRoutes()
+                ->register();
+    }
+
+    /**
+     * Register the Nova gate.
+     *
+     * This gate determines who can access Nova in non-local environments.
+     *
+     * @return void
+     */
+    protected function gate()
+    {
+        Gate::define('viewNova', function ($user) {
+            return in_array($user->email, [
+                //
+            ]);
+        });
+    }
+
+    /**
+     * Get the dashboards that should be listed in the Nova sidebar.
+     *
+     * @return array
+     */
+    protected function dashboards()
+    {
+        return [
+            new \App\Nova\Dashboards\Main,
+        ];
+    }
+
+    /**
+     * Get the tools that should be listed in the Nova sidebar.
+     *
+     * @return array
+     */
+    public function tools()
+    {
+        return [
+            // ...
+            (new \Sereny\NovaPermissions\NovaPermissions())->canSee(function ($request) {
+                return $request->user()->isSuperAdmin();
+            }),
+        ];
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $collection = collect([
+            'Product',
+            'Productcategory',
+            'Productsubcategory',
+            'Productbrand',
+            'Productmodel',
+            'Producttype',
+            'Productunit',
+            'Stock',
+            'StockUpdate',
+            'Section',
+            'Employee',
+            'Designation',
+            'User',
+            'Role',
+            'Permission'
+            // ... // List all your Models you want to have Permissions for.
+        ]);
+
+        $collection->each(function ($item, $key) {
+            // create permissions for each collection item
+            Permission::create(['group' => $item, 'name' => 'viewAny' . $item]);
+            Permission::create(['group' => $item, 'name' => 'view' . $item]);
+            Permission::create(['group' => $item, 'name' => 'update' . $item]);
+            Permission::create(['group' => $item, 'name' => 'create' . $item]);
+            Permission::create(['group' => $item, 'name' => 'delete' . $item]);
+            Permission::create(['group' => $item, 'name' => 'destroy' . $item]);
+        });
+
+        // Create a Super-Admin Role and assign all Permissions
+        $role = Role::create(['name' => 'super-admin']);
+        $role->givePermissionTo(Permission::all());
+
+         //Give User Super-Admin Role
+         $user = \App\Models\User::where('email', 'shuvoworld@gmail.com')->first(); // Change this to your email.
+         $user->assignRole('super-admin');
+    }
+}
